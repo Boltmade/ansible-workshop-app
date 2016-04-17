74d67af515744adced7cf6aae3fd1a0ce9b1d16a
Jesse McGinnis <jesse@jcmcginnis.com>
2016-04-16 22:05:10 -0400

    Introduce #ping endpoint for passenger

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :coins, only: [:index, :create]
   get "reset", to: 'coins#reset'
 
+  get "ping" => "ping#index", constraints: { ip: /::1|127\.0\.0\.1/ }
+
   root "coins#index"
 end
